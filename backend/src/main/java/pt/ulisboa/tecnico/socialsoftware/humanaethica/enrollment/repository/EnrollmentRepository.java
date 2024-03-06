87cc65ccef0f4f62bf19e2825869fdc888d4a19c
Guilherme Ribeiro <guilherme.neca.ribeiro@tecnico.ulisboa.pt>
2024-03-06 11:43:44 +0000

    create folders for enrollment tests and fisrt file

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/enrollment/repository/EnrollmentRepository.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/enrollment/repository/EnrollmentRepository.java
@@ -6,8 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
-import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.domain.Activity;
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.enrollment.domain.Enrollment;
 
 @Repository
@@ -17,8 +15,6 @@ public interface EnrollmentRepository extends JpaRepository<Enrollment, Integer>
 
     @Query("SELECT e FROM Enrollment e WHERE e.activity.id = :activityId")
     List<Enrollment> getEnrollmentsByActivityId(Integer activityId);
-
-    
 
 }
 
