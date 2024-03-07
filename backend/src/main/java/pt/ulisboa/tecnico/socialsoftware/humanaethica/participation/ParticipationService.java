98a1da475f2c7a809ea2b51abf193f13af3b7ff1
WatchYoJet <pedropereira972@gmail.com>
2024-03-07 15:09:43 +0000

    feat: Implement Service Create #52

---
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/participation/ParticipationService.java
@@ -0,0 +1,45 @@
+package pt.ulisboa.tecnico.socialsoftware.humanaethica.participation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
+
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.participation.domain.Participation;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.participation.dto.ParticipationDto;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.participation.repository.ParticipationRepository;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.user.domain.Volunteer;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.exceptions.HEException;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.user.repository.UserRepository;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.domain.Activity;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.repository.ActivityRepository;
+
+import static pt.ulisboa.tecnico.socialsoftware.humanaethica.exceptions.ErrorMessage.*;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ParticipationService {
+    @Autowired
+    ParticipationRepository participationRepository;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    ActivityRepository activityRepository;
+
+    @Transactional(isolation = Isolation.READ_COMMITTED)
+    public ParticipationDto createParticipation(Integer activityId, ParticipationDto participationDto) {
+        if (activityId == null) throw new HEException(ACTIVITY_NOT_FOUND);
+        Activity activity = activityRepository.findById(activityId).orElseThrow(() -> new HEException(ACTIVITY_NOT_FOUND, activityId));
+        Integer userId = participationDto.getVolunteer().getId();
+        Volunteer volunteer = (Volunteer) userRepository.findById(userId).orElseThrow(() -> new HEException(USER_NOT_FOUND, userId));
+        Participation participation = new Participation(activity, volunteer, participationDto);
+
+        participationRepository.save(participation);
+
+        return new ParticipationDto(participation);
+    }
+}
