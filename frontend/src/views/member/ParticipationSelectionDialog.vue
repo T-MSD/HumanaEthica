c39b8ba76ae078018291a50eac51df89407eead6
ist198962 <p.vaz.pereira@tecnico.ulisboa.pt>
2024-03-26 16:00:27 +0000

    feat: Create dialog presentation, initialization and input verification #71 #72 #73

---
+++ frontend/src/views/member/ParticipationSelectionDialog.vue
@@ -0,0 +1,79 @@
+<template>
+  <v-dialog v-model="dialog" persistent width="1300">
+    <v-card>
+      <v-card-title>
+        <span class="headline">
+          Select Participant
+        </span>
+      </v-card-title>
+      <v-card-text>
+        <v-form ref="form" lazy-validation>
+          <v-row>
+            <v-col cols="12">
+              <v-text-field
+                  label="Rating"
+                  :rules="[
+                  (v) =>
+                    isNumberValid(v) ||
+                    'Number of rating must be between 1 and 5',
+                ]"
+                  data-cy="participantsNumberInput"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+        </v-form>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+            color="blue-darken-1"
+            variant="text"
+            @click="$emit('close-participation-dialog')"
+        >
+          Close
+        </v-btn>
+        <v-btn
+            color="blue-darken-1"
+            variant="text"
+            data-cy="saveActivity"
+        >
+          Make Participant
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+<script lang="ts">
+import { Vue, Component, Prop, Model } from 'vue-property-decorator';
+import Participation from '@/models/participation/Participation';
+import RemoteServices from '@/services/RemoteServices';
+import VueCtkDateTimePicker from 'vue-ctk-date-time-picker';
+import 'vue-ctk-date-time-picker/dist/vue-ctk-date-time-picker.css';
+import { ISOtoString } from '@/services/ConvertDateService';
+
+
+@Component({
+  methods: { ISOtoString },
+})
+export default class ParticipationSelectionDialog extends Vue {
+  @Model('dialog', Boolean) dialog!: boolean;
+  @Prop({ type: Participation, required: true }) readonly participation!: Participation;
+  createParticipation: Participation = new Participation();
+
+  cypressCondition: boolean = false;
+
+  async created() {
+    this.createParticipation = new Participation(this.participation);
+  }
+
+  isNumberValid(value: any) {
+    if (value == null) return true;
+    if (!/^\d+$/.test(value)) return false;
+    const parsedValue = parseInt(value);
+    return parsedValue >= 1 && parsedValue <= 5;
+  }
+
+}
+</script>
+
+<style scoped lang="scss"></style>
