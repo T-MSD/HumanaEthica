64263aa2dc5d89fc7c1dc3c0d506e1696f39c24c
Tiago <tiago.rebelo.2003@tecnico.ulisboa.pt>
2024-03-26 22:38:58 +0000

    fix: fixed index on final verification #102 #103

--- frontend/tests/e2e/specs/volunteer/volunteer.js
+++ frontend/tests/e2e/specs/volunteer/volunteer.js
@@ -25,7 +25,7 @@ describe('Volunteer', () => {
         .eq(0).find('[data-cy="writeAssessmentButton"]').click();
     cy.get('[data-cy="descriptionInput"]').type('thisReviewHas10Characters');
     cy.get('[data-cy="saveAssessment"]').click();
-    cy.get('[data-cy="volunteerActivitiesTable"] tbody tr').eq(0).children().eq(3).should('contain', 'thisReviewHas10Characters');
+    cy.get('[data-cy="volunteerActivitiesTable"] tbody tr').eq(0).children().eq(1).should('contain', 'thisReviewHas10Characters');
     cy.logout();
   });
 });
