72ee4c27eec22084554f3ea5ea3b9637146e3f61
Miguel Gomes <miguel.b.gomes@tecnico.ulisboa.pt>
2024-03-27 18:38:13 +0000

    fix: added test waits

--- frontend/tests/e2e/specs/member/member.js
+++ frontend/tests/e2e/specs/member/member.js
@@ -12,23 +12,29 @@ describe('Volunteer', () => {
     cy.demoMemberLogin();
     const NUMBER = '3';
 
+    cy.intercept('GET', '/users/*/getInstitution').as('getInstitutions');
+    cy.intercept('GET', '/activities/*/enrollments').as('getEnrollments');
+    cy.intercept('POST', '/activities/*/participations').as('register');
+
     cy.get('[data-cy="institution"]').click();
     cy.get('[data-cy="activities"]').click();
+    cy.wait('@getInstitutions');
     cy.get('[data-cy="memberActivitiesTable"] tbody tr')
         .should('have.length', 2);
     cy.get('[data-cy="memberActivitiesTable"] tbody tr')
         .eq(0).children().eq(3).should('contain', '1');
     cy.get('[data-cy="memberActivitiesTable"] tbody tr')
         .eq(0).find('[data-cy="showEnrollments"]').click();
+    cy.wait('@getEnrollments');
     cy.get('[data-cy="activityEnrollmentsTable"] tbody tr')
         .should('have.length', 2);
     cy.get('[data-cy="activityEnrollmentsTable"] tbody tr')
         .eq(0).children().eq(2).should('contain', 'false');
-
     cy.get('[data-cy="activityEnrollmentsTable"] tbody tr')
         .eq(0).find('[data-cy="createButton"]').click();
     cy.get('[data-cy="ratingNumberInput"]').type(NUMBER);
     cy.get('[data-cy="makeParticipation"]').click()
+    cy.wait('@register');
 
     cy.get('[data-cy="activityEnrollmentsTable"] tbody tr')
         .eq(0).children().eq(2).should('contain', 'true');
