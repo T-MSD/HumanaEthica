10f825990fd8ca8f599ac988bbe75ea41008dfe3
Tiago Dias <dias.tiago03@gmail.com>
2024-03-27 00:59:32 +0000

    fix: tests for member #102 #103

--- frontend/tests/e2e/specs/member/member.js
+++ frontend/tests/e2e/specs/member/member.js
@@ -1,26 +1,43 @@
 describe('Volunteer', () => {
   beforeEach(() => {
-    //cy.deleteAllbutArs();
-    //cy.createInstitutions();
-    cy.demoMemberLogin()
+    cy.deleteAllButArs();
+    cy.createInstitutions();
+
   });
 
   afterEach(() => {
-    cy.logout();
+
     cy.deleteAllButArs();
   });
 
   it ('check activities', () => {
+
+    cy.demoVolunteerLogin();
+
+
+    cy.get('[data-cy="volunteerActivities"]').click();
+    cy.get('[data-cy="volunteerActivitiesTable"] tbody tr')
+        .should('have.length', 6);
+    cy.get('[data-cy="volunteerActivitiesTable"] tbody tr')
+        .eq(0).children().eq(0).should('contain', 'A1');
+    cy.get('[data-cy="volunteerActivitiesTable"] tbody tr')
+        .eq(0).find('[data-cy="writeAssessmentButton"]').click();
+    cy.get('[data-cy="descriptionInput"]').type('thisReviewHas10Characters');
+    cy.get('[data-cy="saveAssessment"]').click();
+    cy.logout();
+
+    cy.demoMemberLogin()
     cy.get('[data-cy="institution"]').click();
     cy.get('[data-cy="assessments"]').click();
     cy.get('[data-cy="institutionAssessmentsTable"] tbody tr')
         .should('have.length', 1);
     cy.get('[data-cy="institutionAssessmentsTable"] tbody tr').eq(0).children().eq(0).should('contain', 'thisReviewHas10Characters');
-
+    cy.logout();
   });
 
 
   it('close', () => {
+    cy.demoMemberLogin()
     cy.get('[data-cy="institution"]').click();
     cy.get('[data-cy="members"]').click();
 
@@ -29,6 +46,6 @@ describe('Volunteer', () => {
 
     cy.get('[data-cy="institution"]').click();
     cy.get('[data-cy="activities"]').click();
-
+    cy.logout();
   });
 });
